91c88db2abf75324b24b5354635a4cbac5c258b7
networkSocerer <gigattfyhig@gmail.com>
2025-07-25 22:56:32 +0900

    node fe

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `http://localhost:5000/api`,
+  baseURL: `http://182.227.116.167:5555/api`,
   headers: {
     "Content-Type": "application/json",
   },
