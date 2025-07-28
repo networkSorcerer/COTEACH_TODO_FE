28eab3261e226c8efd15841254da2632a219bf8e
networkSocerer <gigattfyhig@gmail.com>
2025-07-28 15:39:26 +0900

    login & signup

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
   return (
