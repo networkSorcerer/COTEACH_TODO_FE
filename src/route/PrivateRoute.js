b8129321612b2787133107790bfe610d6038554f
networkSocerer <gigattfyhig@gmail.com>
2025-07-30 18:44:39 +0900

    update

--- src/route/PrivateRoute.js
+++ src/route/PrivateRoute.js
@@ -1,12 +1,12 @@
+// PrivateRoute.js
 import React from "react";
 import { Navigate } from "react-router-dom";
-const PrivateRoute = (user, children) => {
-  return (
-    <div>
-      user? children:
-      <Navigate to="/login" />
-    </div>
-  );
+
+const PrivateRoute = ({ user, children }) => {
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
 };
 
 export default PrivateRoute;
