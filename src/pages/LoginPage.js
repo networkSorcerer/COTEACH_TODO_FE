a1edfb42cc2366acff9083b4fab6ad0571662643
networkSocerer <gigattfyhig@gmail.com>
2025-07-30 18:30:05 +0900

    update

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Link, useNavigate, Navigate } from "react-router-dom";
